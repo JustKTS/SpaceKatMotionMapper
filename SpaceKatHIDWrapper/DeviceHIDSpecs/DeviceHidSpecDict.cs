0dc1729eaccc007c6dd4ee82ac8a2cd5c1553d7f
kts <kts121506@outlook.com>
2025-12-24 22:03:54 +0800

    v0.2.8Fix1-更新以支持mini的新固件

--- SpaceKatHIDWrapper/DeviceHIDSpecs/DeviceHidSpecDict.cs
+++ SpaceKatHIDWrapper/DeviceHIDSpecs/DeviceHidSpecDict.cs
@@ -21,6 +21,19 @@ public static class DeviceHidSpecDict
             new(3, 1, 0), // LEFT
             new(3, 1, 1)  // RIGHT
         }.AsReadOnly(), 350.0),
+        ["Mini_V1.3"] = new("SpaceKat V3 Mini", [0x256F, 0xC63A], new Dictionary<MotionAxis, AxisSpecs>()
+        {
+            [MotionAxis.X] = new(1, 1, 2, 1),
+            [MotionAxis.Y] = new(1, 3, 4, -1),
+            [MotionAxis.Z] = new(1, 5, 6, -1),
+            [MotionAxis.Pitch] = new(1, 7, 8, -1),
+            [MotionAxis.Roll] = new(1, 9, 10, -1),
+            [MotionAxis.Yaw] = new(1, 11, 12, 1),
+        }.ToFrozenDictionary(), new List<ButtonSpecs>
+        {
+            new(3, 1, 0), // LEFT
+            new(3, 1, 1)  // RIGHT
+        }.AsReadOnly(), 350.0),
         ["MiniE"] = new("SpaceKat V3 MiniE", [0x256F, 0xC638], new Dictionary<MotionAxis, AxisSpecs>()
         {
             [MotionAxis.X] = new(1, 1, 2, 1),
